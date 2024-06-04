bf1ffd1d539e36af7882072429f93e921cf20833
Sam Cole <sam@samcole.me>
2024-06-04 19:57:38 +0000

    Migrate mootd renewal code

---
+++ mootd/renew.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Chance denominator for cow being atypical (not a cow)
+# e.g., if CHANCE=100 then 1/100 chance of atypical cow
+chance=10
+
+# RNG for cow chance
+cow=$((1 + $RANDOM % $chance))
+if [ $cow != 1 ]; then
+	# Use default cow
+	motd=$(fortune | cowsay)
+else
+	# Use random cow from /usr/share/cows/
+	random_cow=$(ls /usr/share/cows/ | shuf -n1)
+	motd=$(fortune | cowsay -f "/usr/share/cows/$random_cow")
+fi
+
+# Write motd
+echo "$motd" > mootd
